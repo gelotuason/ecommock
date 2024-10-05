4dc478b2828ff7fe21e7bb1d820d88c45d36bbd4
gelotuason <gelotuason@gmail.com>
2024-10-06 00:08:18 +0800

    add logout functionality

--- src/lib/features/auth/authSlice.ts
+++ src/lib/features/auth/authSlice.ts
@@ -2,17 +2,20 @@ import { createSlice, PayloadAction, createAsyncThunk } from '@reduxjs/toolkit';
 
 type User = {
     username: string | null
-    password?: string | null
+    password: string | null
+    email?: string | null
 }
 
 type AuthState = {
     isAuthenticated: boolean
+    user: User
     status: 'idle' | 'loading' | 'success' | 'failed'
     error: string | null
 }
 
 const initialState: AuthState = {
     isAuthenticated: false,
+    user: { username: null, password: null, email: null },
     status: 'idle',
     error: null,
 }
@@ -32,7 +35,11 @@ export const login = createAsyncThunk('auth/login', async (loginDetails: User, {
 
         await res.json();
 
-        return true;
+        return {
+            username: loginDetails.username,
+            email: `${loginDetails.username}@email.com`,
+            success: true
+        };
     } catch (err) {
         console.error(err);
         return rejectWithValue('Username or password is incorrect.');
@@ -43,20 +50,24 @@ const authSlice = createSlice({
     name: 'auth',
     initialState,
     reducers: {
-        // login(state, action: PayloadAction<User>) {
-        //     state.isAuthenticated = true;
-        //     state.user = action.payload;
-        // },
-        // logout
+        logout: (state) => {
+            state.isAuthenticated = false;
+        }
     },
     extraReducers: (builder) => {
         builder
             .addCase(login.pending, (state) => {
                 state.status = 'loading';
             })
-            .addCase(login.fulfilled, (state, action: PayloadAction<boolean>) => {
+            .addCase(login.fulfilled, (state, action: PayloadAction<{
+                username: string | null,
+                email: string | null,
+                success: boolean
+            }>) => {
                 state.status = 'success';
-                state.isAuthenticated = action.payload;
+                state.isAuthenticated = action.payload.success;
+                state.user.username = action.payload.username;
+                state.user.email = action.payload.email;
             })
             .addCase(login.rejected, (state, action: PayloadAction<any>) => {
                 state.status = 'failed';
@@ -64,5 +75,7 @@ const authSlice = createSlice({
             })
     }
 });
+
+export const { logout } = authSlice.actions;
 
 export default authSlice.reducer;
