c547f6fa8540acecad263c2ce31fb140acd835a2
gelotuason <gelotuason@gmail.com>
2024-10-07 21:31:37 +0800

    add conditional rendering

--- src/app/wishlist/page.tsx
+++ src/app/wishlist/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Link from "next/link";
 import ProductCard from "@/components/product/product-card";
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
 import { useAppSelector } from "@/lib/hooks";
@@ -8,7 +9,7 @@ export default function Wishlist() {
     const { wishlists } = useAppSelector(state => state.wishlistReducer);
 
     return (
-        <main className="px-4 py-8 flex-1">
+        <main className="px-4 py-8 flex flex-col flex-1">
             <h2 className="text-4xl mb-2 text-center">Wishlist</h2>
             <Breadcrumb className="mb-4 flex justify-center">
                 <BreadcrumbList>
@@ -22,11 +23,15 @@ export default function Wishlist() {
                 </BreadcrumbList>
             </Breadcrumb>
 
-            <section className="grid grid-cols-2 gap-x-3 gap-y-8">
-                {wishlists && wishlists.map(wishlist => (
-                    <ProductCard product={wishlist} />
-                ))}
-            </section>
+
+            {wishlists.length !== 0
+                ? wishlists.map(wishlist => (
+                    <section className="grid grid-cols-2 gap-x-3 gap-y-8">
+                        <ProductCard product={wishlist} />
+                    </section>
+                ))
+                : <p className="text-center mt-10">No products were added to the wishlist. <Link href='/shop' className="underline">Back to shopping.</Link></p>
+            }
         </main>
     )
 }
